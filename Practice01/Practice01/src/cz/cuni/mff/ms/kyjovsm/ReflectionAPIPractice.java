db2dced6c9d77bdac6be7b571f56e0d5a9af2b47
Michal Kyjovsky <michalkyjovsky@hotmail.com>
2020-02-19 13:54:45 +0100

    First practice.

---
+++ Practice01/Practice01/src/cz/cuni/mff/ms/kyjovsm/ReflectionAPIPractice.java
@@ -0,0 +1,12 @@
+package cz.cuni.mff.ms.kyjovsm;
+
+public class ReflectionAPIPractice {
+    private void sayHello(){
+        System.out.println("Hello fellas from private world!!");
+    }
+
+    private static void sayStaticHello(){
+        System.out.println("Hello fellas from private world!!");
+    }
+
+}
